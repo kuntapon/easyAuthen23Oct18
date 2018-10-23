3bf8f80a502b1a5fdaf8620bb11bf6e97bbcb375
mrkun <kuntapon55@gmail.com>
2018-10-23 14:01:49 +0700

    Get Value from text

--- easyAuthen_23Oct18/ViewController.swift
+++ easyAuthen_23Oct18/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+//    Explicit
+    var userString: String = ""
+    var pswString: String = ""
+    
+    
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    @IBAction func btnSignin(_ sender: Any) {
+        
+        userString = txtUsername.text!
+        pswString = txtPassword.text!
+        
+    //Show log
+        print("user ==> \(userString)  password ==> \(pswString)")
+        
+        
+    }   //singin Function
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
