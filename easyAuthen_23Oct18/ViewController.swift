f376794f45286764a872f1e8cd4837743dfaad9d
mrkun <kuntapon55@gmail.com>
2018-10-23 13:26:02 +0700

    UI Near OK

---
+++ easyAuthen_23Oct18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easyAuthen_23Oct18
+//
+//  Created by Kuntapon Tanikkool on 23/10/2561 BE.
+//  Copyright © 2561 Kuntapon Tanikkool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
